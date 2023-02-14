4501b3b04d6fffb640f1b9216374d2b864728bde
Ricardostf <ricardosantos.ook@gmail.com>
2023-02-14 16:40:36 -0300

    att 14-02

--- server/servidor.js
+++ server/servidor.js
@@ -8,10 +8,17 @@ const servidor = express()
 servidor.use(express.json())
 servidor.use(express.urlencoded({extended: true}))
 
+servidor.get("/login", telaInicial)
+
+
 servidor.use("/client", clienteRouter)
 
 servidor.listen(3000, servico)
 
 function servico(){
     console.log("Servidor rodando...")
+}
+
+function telaInicial(req, res){
+    res.sendFile("./view/emprestimo.html", { root: '.' })
 }
