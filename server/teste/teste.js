7674674a2df21f1a1a15beeb35c5f41ce85cd4a3
Diego Marques <diegodesouzamarques@gmail.com>
2023-03-01 02:04:39 -0300

    28/03/2023 Create casos de Teste + testes

--- server/teste/teste.js
+++ server/teste/teste.js
@@ -8,33 +8,33 @@ async function test_case(){
     await driver.get("http://localhost:3000/livro/");
 
     await driver.findElement(By.name("nomeBuscado")).sendKeys("livro nao cadastrado", Key.RETURN);
-
-
+    await driver.sleep(4000);
+    test_case2();
 }
 
 async function test_case2(){
     //teste de nome válido
+    
     let driver = await new Builder().forBrowser("chrome").build();
-
-    await driver.get("http://localhost:3000/livro/");
-
-    await driver.findElement(By.name("nomeBuscado")).sendKeys("O pequeno principe", Key.RETURN);
-}
-
-
-async function test_case3(){
-    //teste de deletar livro
-    let driver = await new Builder().forBrowser("chrome").build();
-
     await driver.get("http://localhost:3000/livro/");
 
     await driver.findElement(By.name("nomeBuscado")).sendKeys("O pequeno principe", Key.RETURN);
     await driver.sleep(5000);
+    test_case3();
+}
+
+
+async function test_case3(){
+    //teste de cadasrar livro existente
     
-    await driver.findElement(By.id('deleteButton')).click();
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("http://localhost:3000/livro/");
 
-
-    await botao.click();
+    await driver.findElement(By.name("nome")).sendKeys("O pequeno principe");
+    await driver.sleep(2000);
+    await driver.findElement(By.name("autor")).sendKeys("um autor");
+    await driver.sleep(2000);
+    await driver.findElement(By.name("editora")).sendKeys(1, Key.RETURN);
 
     setInterval(function(){
         driver.quit();
@@ -42,4 +42,4 @@ async function test_case3(){
 }
 
 test_case();
-test_case2();
+
